fb2d1bcf87b3cb76bebe6e14a2b44d052780500d
Rohit Sahoo <rohitsahoo112003@gmail.com>
2025-10-25 23:12:25 +0530

    main file changes

--- src/main/java/com/libraryManagementSystem/LibraryManagementSystemApplication.java
+++ src/main/java/com/libraryManagementSystem/LibraryManagementSystemApplication.java
@@ -1,13 +1,28 @@
 package com.libraryManagementSystem;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication
 public class LibraryManagementSystemApplication {
+	
+	@Value("${spring.datasource.url:NOT_SET}")
+    private String dbUrl;
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LibraryManagementSystemApplication.class, args);
+		
 	}
+	
+	@PostConstruct
+    public void logDbUrl() {
+        System.out.println("spring.datasource.url = " + dbUrl);
+    }
+
 
 }
